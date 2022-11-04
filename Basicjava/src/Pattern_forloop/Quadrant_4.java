e32ea9912f2e3e201da6c3658cc59f9e2b82dcb0
Snehal Nimase <snehu667@gmail.com>
2022-11-04 11:28:03 +0530

    change

--- Basicjava/src/Pattern_forloop/Quadrant_4.java
+++ Basicjava/src/Pattern_forloop/Quadrant_4.java
@@ -4,7 +4,7 @@ public class Quadrant_4 {
 	public static void main(String[] args) {
 		int star=5;
 		
-		for( int i=1;i<=7;i++) {
+		for( int i=1;i<=5;i++) {
 			for (int j=1;j<=star;j++) {
 				System.out.print("*");
 				
@@ -12,7 +12,7 @@ public class Quadrant_4 {
 			System.out.println();
 			
 		
-		star--;
+		star++;
 		}
 	}
 }
